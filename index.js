7eea8127fa64da8ac56636cd16c04a42496eea20
Thierry Delbart <thierry@thde.pro>
2016-11-17 00:26:54 -0200

    Adding server basic structure

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+// BASE SETUP
+// =============================================================================
 
+// the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
